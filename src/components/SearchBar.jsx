0094f30e5422eb8ded5cd1d00b9fb9ef56d06625
Thonny Cardenas <thonnycardenas@gmail.com>
2024-09-30 13:18:18 +0200

    fin

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import CarritoContext from './../context/CarritoContext';
-import './SearchBar.scss';
+import 'src/components/searchBar.scss';
 
 const SearchBar = () => {
-  
   const [searchQuery, setSearchQuery] = useState("");
   const navigate = useNavigate();
   const { cartCount } = useContext(CarritoContext);
@@ -18,12 +17,12 @@ const SearchBar = () => {
 
   return (
     <div className="search-bar">
-      {}
+      {/* Logo Container */}
       <div className="search-bar__logo-container">
-        <img className="logo-carro-1" src="desktop-solid.svg" width="200" height="70" alt="LAPTOZONE" />
+        <img className="logo-carro-1" src="desktop-solid.svg" alt="LAPTOZONE" />
       </div>
 
-      {}
+      {/* Search Form */}
       <form className="search-bar__form-container" onSubmit={handleSearchSubmit}>
         <label htmlFor="busqueda" className="search-bar__form-label">Buscar</label>
         <input
@@ -36,24 +35,24 @@ const SearchBar = () => {
         <input type="submit" className="search-bar__form-submit" value="Buscar" />
       </form>
 
-      {}
+      {/* Cart Container */}
       <div className="search-bar__carrito-container">
         <Link to="/carrito">
-          <img className="logo-carro" src="cart-shopping-solid.svg" alt="Carrito" width="40" height="40" />
-          <span className="carrito-count">{cartCount}</span> {}
+          <img className="logo-carro" src="cart-shopping-solid.svg" alt="Carrito" />
+          <span className="carrito-count">{cartCount}</span>
         </Link>
       </div>
 
-      {}
-      <div className="menu-toogle">
-        <label htmlFor="menu" className="menu-toogle__label">
-          <span className="menu-toogle__top-bread"></span>
-          <span className="menu-toogle__meat"></span>
-          <span className="menu-toogle__bottom-bread"></span>
+      {/* Menu Toggle */}
+      <div className="menu-toggle">
+        <label htmlFor="menu" className="menu-toggle__label">
+          <span className="menu-toggle__top-bread"></span>
+          <span className="menu-toggle__meat"></span>
+          <span className="menu-toggle__bottom-bread"></span>
         </label>
       </div>
     </div>
   );
 };
 
-export default SearchBar
+export default SearchBar;
